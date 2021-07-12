2419945df409ee101d94f439947a942292aae9ca
FALVO <anthony.falvo.pro@gmail.com>
2021-07-12 16:04:39 +0200

    debut js

---
+++ source/js/index.js
@@ -0,0 +1,19 @@
+(function() {
+
+    // ##########  Variable and constante : ##########
+
+    const toggleButton = document.getElementById('toggle-button');
+    const sideBar = document.getElementById('side-bar');
+
+    // ##########  My FUNCTION : ##########
+
+    function show() {
+
+
+
+    };
+
+    // ##########  LISTENER  : ##########
+    toggleButton.addEventListener('click', show);
+
+})();
