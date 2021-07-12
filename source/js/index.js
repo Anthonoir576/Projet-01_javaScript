67c763edb1127aa814ef7f2695bc1700c308246f
FALVO <anthony.falvo.pro@gmail.com>
2021-07-12 16:58:12 +0200

    end - juste la transition a la fin qui sapplique pas

--- source/js/index.js
+++ source/js/index.js
@@ -4,16 +4,25 @@
 
     const toggleButton = document.getElementById('toggle-button');
     const sideBar = document.getElementById('side-bar');
+    const content = document.querySelector('.content');
 
     // ##########  My FUNCTION : ##########
 
     function show() {
 
         sideBar.classList.toggle('active');
+        content.classList.toggle('active2');
 
     };
 
     // ##########  LISTENER  : ##########
     toggleButton.addEventListener('click', show);
+
+    content.addEventListener('click', () => {
+
+        sideBar.classList.remove('active');
+        content.classList.remove('active2');
+
+    });
 
 })();
